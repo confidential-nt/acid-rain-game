72c31117b39986c4fe5d8b61dfcc55ef3587b76b
confidential-nt <54153087+confidential-nt@users.noreply.github.com>
2020-11-04 04:22:21 +0900

    update 2

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const gameForm = document.querySelector(".game-form");
 
 let words = ["Star","Tree", "Airplane", "Tail", "Basketball",  "Mouth", "Telephone", "Chin", "Jar",  "Smile", "Cheek", "Ear", "Drum","Room", "Turtle","Wings","Doll","Bird","Spider","Hopscotch","Happy","Baby","Monkey","Pig","Jump","Crayon", "Arm","Arm","Rabbit", "Book", "Camera", "Rock", "Chicken", "Robot", "Drink","Balloon","Kangaroo","Clap","Baseball","Milk","Icecream", "Circle","Book","Sneeze","Dog","Flower","Pillow","Sleep","Spoon","Skip","Football", "Kick","Head","Sunglasses","Mosquito","Pinch","Chair","Jump"]
 let proceed = 0;
+// let randomSelectedWords = [];
 // const unit = "px";
-let ads = [];
+let funcArray = [];
 let first = true;
 let done = 0;
 let randomWords = [];
@@ -54,8 +55,8 @@ function rainWord(number, word){
     if(first === true){
         containers[number].innerText = word
     }else{
-        containers[number-1].innerText = "";
-        containers[number].innerText = word
+        containers[number-(3-(2*number))].innerText = "";
+        containers[number+(2*number)].innerText = word
     }
 
     
@@ -121,17 +122,18 @@ function showRandomWords(){
     const randomNum = Math.floor(Math.random() * (words.length + 1));
     const randomWord = words[randomNum];
     if(done == 0){
-        for(let i = 0; i < gameView.offsetHeight/20; i++){
+        for(let i = 0; i < (gameView.offsetHeight/20)*3; i++){
             const div = document.createElement("div")
+            div.id = i + 1;
             div.classList.add("container");
             // div.innerText = randomWord;
             gameView.appendChild(div);
         }
     }
    
-    const wordSpan = document.createElement("span");
-    const wordDiv = document.createElement("div")
-    wordDiv.classList.add("moving");
+    // const wordSpan = document.createElement("span");
+    // const wordDiv = document.createElement("div")
+    // wordDiv.classList.add("moving");
     // // ads.push(wordDiv);
     // // const divId = wordDiv.length + 1;
     // // wordDiv.id = divId;
@@ -139,15 +141,22 @@ function showRandomWords(){
     // wordSpan.innerText = randomWord;
     // // wordSpan.style.setProperty("transform", `translateY(${8*proceed}px)`)
     // // wordSpan.id = Math.floor(Math.random() * 30);
-    wordDiv.appendChild(wordSpan);
+    // wordDiv.appendChild(wordSpan);
  
-    const container = document.querySelector(".container");
+    // const container = document.querySelector(".container");
 
-    container.appendChild(wordDiv);
+    // container.appendChild(wordDiv);
 
     randomWords.push(randomWord)
+    funcArray.push(function (){
+        return randomWords[proceed];
+    })
 
-    rainWord(proceed, randomWords[0]);
+    funcArray.forEach(func =>{
+        rainWord(proceed, func());
+    })
+
+    
     done++;
     proceed++;
 }
